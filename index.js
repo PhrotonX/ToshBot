3f224c2a40feda1a66ec53de57c4f2dbaa88ebda
PhrotonX <cpascual185@gmail.com>
2022-07-31 12:30:20 +0800

    Build 3: Add 3 basic commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//TOSHBOT v0.1.0.1 alpha build 2
+//TOSHBOT v0.1.0.1 alpha build 3
 
 // Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('discord.js');
@@ -11,6 +11,20 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+client.on('interactionCreate', async interaction =>{
+	if(!interaction.isChatInputCommand()) return;
+
+	const {commandName} = interaction;
+
+	if(commandName === 'ping'){
+		await interaction.reply('Pong!');
+	}else if(commandName === 'server'){
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}else if(commandName === 'user'){
+		await interaction.reply('User info.');
+	}
+})
 
 // Login to Discord with your client's token
 client.login(token);
