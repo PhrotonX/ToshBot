4b103faf2883289c6ffd0a507664cd50d7b8940a
PhrotonX <cpascual185@gmail.com>
2022-07-31 14:52:14 +0800

    Build 7: Add event handling
    Include two files: event/interactionCreate.js and event/ready.js.
    Contains an error which includes undefined variable c, resulting into an error in the bot. The bot stopped responding to commands.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//TOSHBOT v0.1.0.1 alpha build 6
+//TOSHBOT v0.1.0.1 alpha build 7
 
 const fs = require('node:fs');
 const path = require('node:path');
@@ -10,6 +10,8 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for(const file of commandFiles){
 	const filePath = path.join(commandsPath, file);
@@ -17,23 +19,22 @@ for(const file of commandFiles){
 	client.commands.set(command.data.name, command);
 }
 
+for(const file of eventFiles){
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if(event.once){
+		client.once(event.name, (...args) => event.execute(...args));
+	}else{
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if(!command) return;
-
-	try{
-		await command.execute(interaction);
-	}catch (error){
-		console.error(error);
-		await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
-	}
+	console.log(`${interaction.user.tag} in#${interaction.channel.name} triggered an interaction.`);
 });
 
 client.login(token);
