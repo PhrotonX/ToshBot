6b7c63b6e10c715242f3c662eb1271fe5195addb
PhrotonX <cpascual185@gmail.com>
2022-07-31 13:44:48 +0800

    Build 5: Code rewrite due to an error
    The application does not respond in a discord server.

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-//TOSHBOT v0.1.0.1 alpha build 4
+//TOSHBOT v0.1.0.1 alpha build 5
 
-// Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction =>{
-	if(!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-	const {commandName} = interaction;
+	const { commandName } = interaction;
 
-	if(commandName === 'ping'){
+	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
-	}else if(commandName === 'server'){
+	} else if (commandName === 'server') {
 		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	}else if(commandName === 'user'){
-		await interaction.reply('User info.');
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
 
-// Login to Discord with your client's token
 client.login(token);
