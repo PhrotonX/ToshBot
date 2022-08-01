9be7ec1518bbf9a73cb4cd181e447d9a765ea61f
PhrotonX <cpascual185@gmail.com>
2022-08-01 11:46:10 +0800

    Build 14: Fix /about command
    Display /about dommand, but it displays incorrect build number 13.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//TOSHBOT v0.1.0.1 alpha build 13
+//TOSHBOT v0.1.0.1 alpha build 14
 
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const { version } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
