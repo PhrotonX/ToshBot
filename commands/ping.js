79a1599cd262016d2ac4063373fb23f30e1a59cd
PhrotonX <cpascual185@gmail.com>
2022-08-10 14:29:21 +0800

    Build 19: Add editing response to /ping command.

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,13 @@
 const {SlashCommandBuilder} = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction){
-        return interaction.reply({content: 'Pong!', ephemeral: true});
+        await interaction.reply({content: 'Pong!', ephemeral: true});
+        await wait(2000);
+        await interaction.editReply('Pong again!');
     },
 };
